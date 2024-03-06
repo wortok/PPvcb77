9345a00bf0d0ebc926def0b7ab7cc9437b51df59
Baryga55 <121411090+Baryga55@users.noreply.github.com>
2024-03-06 09:52:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var paragraph = document.createElement("p");
 paragraph.id = "your_id_here";
 
 // Set the text content of the paragraph element
-paragraph.textContent = "Your dynamic text here";
+paragraph.textContent = "ppworlde@gmail.com";
 
 // Append the paragraph element to the placeholder div
 document.getElementById("dynamic-text").appendChild(paragraph);
