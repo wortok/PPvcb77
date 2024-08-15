660da18c15ab199ef575d932ec346c097c248bd7
wortok <142085396+wortok@users.noreply.github.com>
2024-08-15 10:04:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     // Create a list of email addresses
     var emails = [
-        "ppgoodes@gmail.com",
-        "ppgoodes@gmail.com",
-        "ppgoodes@gmail.com",
-        "ppgoodes@gmail.com"
+        "ppboopet@gmail.com",
+        "ppboopet@gmail.com",
+        "ppboopet@gmail.com",
+        "ppboopet@gmail.com"
     ];
 
     // Generate a random index
