b35c238873f46d7e5c23fb474b1089ea08a8bdd7
Baryga55 <121411090+Baryga55@users.noreply.github.com>
2024-09-29 13:40:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function() {
     // Create a list of email addresses
     var emails = [
-        "ppnerfs@zohomail.eu",
-        "ppnerfs@zohomail.eu",
-        "ppnerfs@zohomail.eu",
-        "ppnerfs@zohomail.eu"
+        "ppwoopie@gmail.com",
+        "ppwoopie@gmail.com",
+        "ppwoopie@gmail.com",
+        "ppwoopie@gmail.com"
     ];
 
     // Generate a random index
